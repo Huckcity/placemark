0eadc7e1021cbd11a87c3465da8464f9aa3c496e
Adam Gibbons <adam.p.gibbons@gmail.com>
2022-05-21 14:46:32 +0100

    Adding HTML validation for security tests

--- models/mongo/user.js
+++ models/mongo/user.js
@@ -48,6 +48,14 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+userSchema.pre("findByIdAndUpdate", async function (next) {
+  if (this._update.password) {
+    const hash = await bcrypt.hash(this._update.password, 10);
+    this._update.password = hash;
+  }
+  next();
+});
+
 userSchema.methods.checkPassword = async function (password) {
   const isMatch = await bcrypt.compare(password, this.password);
   return isMatch;
